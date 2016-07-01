233be6139c9ddbdb06dfd0a5151f9dd47df2387f
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-07-01 17:39:00 -0600

    Clases para los nuevos layouts

---
+++ app/src/main/java/com/example/arciniega/proyecto07_navigator/Activity01.java
@@ -0,0 +1,13 @@
+package com.example.arciniega.proyecto07_navigator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Activity01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout01);
+    }
+}
